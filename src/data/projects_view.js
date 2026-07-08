7bfaf5d2077fb87a4baf93ca83ad5ed2b909e362
monterocamilo99@hotmail.com <monterocamilo99@hotmail.com>
2026-07-08 11:53:11 +0200

    enlazando redes sociales a los diferentes botones de acceso

--- src/data/projects_view.js
+++ src/data/projects_view.js
@@ -1,29 +1,35 @@
+import { Link } from "lucide-astro";
+import barberapi from "../assets/projects/barberapi.webp";
+import biblioteca from "../assets/projects/biblioteca.webp";
+import portfolio from "../assets/projects/portfolio.webp";
+import apiconsumer from "../assets/projects/getpostman.webp";
+
 export const projects = [
   {
-    title: "E-Commerce Platform",
+    title: "Biblioteca API",
     description:
-      "Plataforma completa de comercio electrónico con carrito de compras, pagos y panel de administración.",
-    tags: ["React", "Node.js", "MongoDB", "Stripe"],
-    image: "🛒",
-    github: "#",
+      "API de biblioteca desarrolada en java que permite consultar, gestionar y visualizar informacion de libros, trabajando la persistencia de datos.",
+    tags: ["Java", "JAVA FX"],
+    image: biblioteca,
+    github: "https://github.com/juancamilo99-prog/BibliotecaAPI",
     demo: "#",
   },
   {
-    title: "Task Management App",
+    title: "Portfolio Web",
     description:
-      "Aplicación de gestión de tareas con drag & drop, colaboración en tiempo real y notificaciones.",
-    tags: ["Next.js", "TypeScript", "Prisma", "WebSockets"],
-    image: "📋",
-    github: "#",
+      "Más que un portfolio, este proyecto representa mi evolución como desarrollador. Un espacio donde comparto los proyectos, tecnologías y aprendizajes que forman parte de mi camino hacia el desarrollo de software.",
+    tags: ["JavaScript", "Astro", "GSAP", "Tailwind"],
+    image: portfolio,
+    github: "https://github.com/juancamilo99-prog/portfolio-astro-tailwind",
     demo: "#",
   },
   {
     title: "Barber API",
     description:
       "Sistema de reservas para barberia.",
-    tags: ["Next.js", "TypeScript", "Prisma"],
-    image: "✂️",
-    github: "#",
+    tags: ["HTML", "JavaScript", "CSS", "Java", "Spring Boot"],
+    image: barberapi,
+    github: "https://github.com/juancamilo99-prog/BarberAPI-Intermodular",
     demo: "#",
   },
   {
@@ -31,8 +37,8 @@ export const projects = [
       description:
         'Generador de portfolios con plantillas personalizables y exportación a código.',
       tags: ['Next.js', 'Tailwind', 'MDX'],
-      image: '🎨',
-      github: '#',
+      image: apiconsumer,
+      github: 'https://github.com/juancamilo99-prog/Proyecto-5-NoSQL---Juan-Camilo-Montero',
       demo: '#',
   }
 ];
